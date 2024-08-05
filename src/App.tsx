d56ae0ffbf786cc460f614fef3b6f9d0a46dfc46
DmitryPanikarovsky <dmitry.panikarovsky@yandex.ru>
2024-08-05 17:43:13 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Link, Route, Routes } from 'react-router-dom'
 import { Counter } from './component/Counter';
 import './index.scss';
+import AboutPage from './pages/AboutPage/AboutPage'
+import MainPage from './pages/MainPage/MainPage'
 
 const App = () => {
     return (
         <div className='app'>
-            jhghgjjg
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Routes>
+                <Route path={'/'} element={<MainPage/>}/>
+                <Route path={'/about'} element={<AboutPage/>}/>
+            </Routes>
         </div>
     );
 };

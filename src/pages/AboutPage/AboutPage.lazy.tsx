b6254e6972460353436da4641b04829e3018364c
DmitryPanikarovsky <dmitry.panikarovsky@yandex.ru>
2024-08-06 12:56:02 +0300

    add fallback for lazy

--- src/pages/AboutPage/AboutPage.lazy.tsx
+++ src/pages/AboutPage/AboutPage.lazy.tsx
@@ -1,4 +1,7 @@
 import { lazy } from 'react';
 
-export const AboutPageLazy = lazy(() => import('./AboutPage'));
+export const AboutPageLazy = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    setTimeout(() => resolve(import("./AboutPage")), 1500);
+}));
 

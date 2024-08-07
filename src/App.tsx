89b81dd09ccf7d21b078adf50e1b3b812a914e84
DmitryPanikarovsky <dmitry.panikarovsky@yandex.ru>
2024-08-07 17:15:58 +0300

    add toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
 
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
+
     return (
-        <div className='app'>
+        <div className={` app ${theme}`}>
+            <button onClick={toggleTheme}>theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>

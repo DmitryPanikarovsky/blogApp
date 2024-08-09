e94dc31a42612b19a24da3b02829597c50f270bf
DmitryPanikarovsky <dmitry.panikarovsky@yandex.ru>
2024-08-09 09:56:15 +0300

    wrote a lib classNames

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { Suspense, useContext } from 'react';
-import { Theme, ThemeContext } from './theme/ThemeContext'
+import { Suspense } from 'react';
 import { useTheme } from './theme/useTheme'
+import { clNames } from './helpers/classNames/classNames'
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={` app ${theme}`}>
+        <div className={clNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
